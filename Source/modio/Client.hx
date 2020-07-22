c2b8470dfa6fb29dcfe571abe07f640c5534a25a
Apprentice Alchemist <azzer159@yahoo.com>
2020-07-22 16:29:21 +0200

    .

--- Source/modio/Client.hx
+++ Source/modio/Client.hx
@@ -1,12 +1,25 @@
 package modio;
 
+import haxe.http.HttpMethod;
+import modio.endpoints.Endpoints;
+
 class Client {
-    public var token:String;
+	public static var inst:Client;
 
-    public static function new(tkn:String){
+	public var token:String;
+	public var endpoints:Endpoints;
 
+	public function new(tkn:String) {
+		token = tkn;
+        inst = this;
+        endpoints = new Endpoints(this);
     }
-    public function initFolder(){
-
+    public function emailRequest(mail:String,cb:Int -> Void){
+        var req = new haxe.Http("https://api.mod.io/v1/oauth/emailrequest");
+        req.addParameter("api_key",token);
+        req.addParameter("email",mail);
+        req.addHeader("Content-Type", "application/x-www-form-urlencoded");
+        throw new haxe.Exception("Not supported yet!");
+        
     }
 }
--- tools/Main.hx
+++
@@ -1,5 +0,0 @@
-class Main {
-    public function main(){
-        Sys.printLn("ModioHx does not have any cli tools.");
-    }
-}
